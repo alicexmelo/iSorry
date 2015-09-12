d98a905db668964c216b57b92c17ddee62052954
Alice Melo <alicemelo89@gmail.com>
2015-09-12 16:39:22 -0300

    mais desculpas

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'forgery'
-Mensagem.create( tipo: 'dei bolo' , conteudo: Forgery(:lorem_ipsum).words(30))
+Mensagem.create( tipo: 'dei bolo' , conteudo: '“Se eu não lavar a roupa, eu vou literalmente ter que sair de casa pelado amanhã”.')
+Mensagem.create( tipo: 'dei bolo' , conteudo: "“Hum, internet? Talvez você tenha ouvido falar sobre isso?”")
+Mensagem.create( tipo: 'dei bolo' , conteudo: "“Eu tenho um encontro quente com um pedaço de pizza”")
+
+Mensagem.create( tipo: 'Ofendi' , conteudo: '“malz ae."')
+Mensagem.create( tipo: 'Ofendi' , conteudo: "“Não sei perder minha vida!! Não sei qual a minha culpa mas, peço perdão.”")
+Mensagem.create( tipo: 'Ofendi' , conteudo: "“Eu te peço perdão por te amar de repente!! Embora o meu amor seja uma velha canção nos teus ouvidos”")
+
+Mensagem.create( tipo: 'Foi Sem Querer' , conteudo: '“Foi sem querer querendo!!”')
+Mensagem.create( tipo: 'Foi Sem Querer' , conteudo: "“Desculpa mesmo por te amar!! FOI SEM QUERER.”")
+Mensagem.create( tipo: 'Foi Sem Querer' , conteudo: "“Como todas as coisas da vida, sem querer pedimos desculpas.”")
