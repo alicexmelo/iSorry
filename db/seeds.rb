a4446daa5a96f7351a8aca14f2748add638def64
Alice Melo <alicemelo89@gmail.com>
2015-09-12 14:45:51 -0300

    adicionado gem lorem ipsum

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'forgery'
+Mensagem.create( tipo: 'dei bolo' , conteudo: Forgery(:lorem_ipsum).words(30))
